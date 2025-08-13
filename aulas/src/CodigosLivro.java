9bcba6d7b3b2f8434e82c0a2fb2503b259a70511
Davi Castro <davic.b.c.r@gmail.com>
2025-08-13 11:02:16 -0300

    exercicios feitos na aula

---
+++ aulas/src/CodigosLivro.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CodigosLivro {
+
+
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+
+    // resolução dos exercicios da poagina 50
+        int c, thisIsAVariable, q76354, number;
+            number = 3;
+
+        System.out.print("Enter an integer: ");
+            int value = entrada.nextInt();
+
+            System.out.println("This is a java program ");
+            System.out.printf("%s%n%s%n", "This is a java", "program");
+
+
+        if (number != 7 )
+            System.out.printf("%d is different than 7", number);
+
+
+
+    }
+
+}
--- aulas/src/aula01.java
+++ aulas/src/aula01.java
@@ -1,4 +1,49 @@
-public class aula01 {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class aula01
+{
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+       /*
+        System.out.println("Digite seu nome: ");
+
+        String nome = sc.nextLine();
+        System.out.println("Seu nome: " + nome);
+
+        */
+
+        /*
+        int[] a = new int[4];
+        int soma = 0;
+
+        System.out.printf("Digite 4 numeros (inteiros), para serem somados: ");
+            for (int i = 0; i < a.length; i++)
+                {
+                    a[i] = sc.nextInt();
+                }
+
+            for (int i = 0; i < a.length; i++)
+                {
+                    soma += a[i];
+                }
+
+        System.out.println("Resultado: " + soma);
+        */
+
+        /* exercicio 2
+        float[] notas = new float[3];
+        float media;
+        System.out.println("Digite as notas para calcular a média aritmetica:  ");
+
+        for (int i = 0; i < notas.length; i++)
+        {
+            notas[i] = sc.nextFloat();
+        }
+
+        media = (notas[0] + notas[1] + notas[2]) / 3;
+        System.out.println("valor da média: " + media);
+        */
     }
 }
