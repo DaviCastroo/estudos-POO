07f6a1a1dd01733f6fb076d05b5487512fe0eca5
Davi Castro <davic.b.c.r@gmail.com>
2025-08-13 16:50:44 -0300

    Exercicios completos

--- aulas/src/aula01.java
+++ aulas/src/aula01.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class aula01
-{
-    public static void main(String[] args)
-    {
+public class aula01 {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
        /*
@@ -14,7 +12,7 @@ public class aula01
 
         */
 
-        /*
+        /* Exercicio 1
         int[] a = new int[4];
         int soma = 0;
 
@@ -32,7 +30,7 @@ public class aula01
         System.out.println("Resultado: " + soma);
         */
 
-        /* exercicio 2
+        /* Exercicio 2
         float[] notas = new float[3];
         float media;
         System.out.println("Digite as notas para calcular a média aritmetica:  ");
@@ -45,5 +43,63 @@ public class aula01
         media = (notas[0] + notas[1] + notas[2]) / 3;
         System.out.println("valor da média: " + media);
         */
+
+        /* Exercicio 3
+        float [] notas = new float [3];
+        float [] pesos = new float [3];
+        float mPonderada = 0 ;
+
+        System.out.println("Digite a seguir as notas, com seus respectivos pesos: ");
+
+        for (int i = 0; i < notas.length; i++)
+        {
+            notas[i] = sc.nextFloat();
+            pesos[i] = sc.nextFloat();
+        }
+        for (int i = 0; i < notas.length; i++)
+        {
+            mPonderada += notas[i] * pesos[i];
+        }
+            mPonderada = mPonderada/ notas.length;
+
+        System.out.println("Média ponderada: " + mPonderada);
+        */
+
+        /* Exercicio 4
+        float salario;
+        double aumento = 0.25;
+
+        System.out.println("Digite o salário do colaborador: ");
+            salario = sc.nextFloat();
+            salario += salario*aumento;
+        System.out.print(salario);
+        */
+
+        /* Exercicio 5
+        float salario;
+        float PercentualAumento;
+
+        System.out.println("Digite a seguir o salário do colaborador, e a porcentagem de seu aumento (Ex: 50): ");
+
+        salario = sc.nextFloat(); PercentualAumento = sc.nextInt();
+        salario += salario * (PercentualAumento/100);
+
+        System.out.print("Salário após o aumento: " + salario);
+        */
+
+        /* Exercicio 6
+        float salario;
+        float GratificacaoSalarial = 5;
+        float imposto = 7;
+
+        System.out.println("Digite o sálario do colaborador: ");
+        salario = sc.nextFloat();
+
+        salario += (salario * (GratificacaoSalarial / 100));
+        salario -= salario * (imposto / 100);
+
+        System.out.println("O salário com a gratificação, ja descontando os impostos é de: R$" + salario);
+        */
+
     }
 }
